a92a956f2e3f39eb11c4c29d908ec241363417cb
lrs <liruishan317@163.com>
2022-06-29 12:21:50 +0000

    tool/

---
+++ tool/data_deal.py
@@ -0,0 +1,70 @@
+import torch
+import numpy as np 
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+import spacy
+
+# ---------简单的数据z-score-----------------------------#
+def z_score(x):
+  #-----------------------------------------------------#
+  # 为数据进行归一化，归一化的数据是经过特征concat之后的数据
+  #-----------------------------------------------------#
+
+  x = (x - np.mean(x)) / np.std(x)
+    
+  return x
+
+
+#———————————————————modified  by lrs at 6/18-----------------------##
+def z_score_1(data):
+  '''
+  ## data : 数组
+     axis : 纬度
+  '''
+  output_data = stats.zscore(data,axis=1)
+  
+  return output_data
+#———————————————————modified  by lrs at 6/18-----------------------##
+  pass
+
+
+def adjust_lr_rate(optimizer, epoch, start_lr):
+  	#学习率动态衰减****增加
+        #lr = start_lr * (1-epoch/Epoch)**0.9
+        # lr = start_lr * min((epoch+1)/150,1)
+        if epoch <20:
+          lr = start_lr
+        elif epoch>=20 and epoch <50:
+          lr = start_lr *0.5
+        elif epoch >=50 and epoch < 120 :
+          lr = start_lr * 0.1
+        elif epoch>=120 and epoch <250:
+          lr = start_lr *0.05
+        elif epoch>=250 and epoch <400 :
+          lr = start_lr * 0.01
+        else :
+          lr = start_lr *0.005
+        # lr = start_lr * (0.1 * (epoch // 3))
+        for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
+
+
+def getDict():
+
+  
+
+
+
+
+  pass
+if __name__ == '__main__' :
+
+  nlp = spacy.load('zh_core_web_sm')
+  doc = nlp(u'我是一个中国人,我爱中国')
+  for token in doc :
+    print(token,token.pos_,token.pos)
+
+
+
+  pass
---
+++ tool/k_Cross.py
@@ -0,0 +1,48 @@
+import numpy as np
+from sklearn.model_selection import KFold
+import os
+import pickle
+
+def k_cross():
+    '''
+        返回10个不同的lab序列
+    '''
+    feature_path = r'/home/lrs/dengfeng.p/break_feature/Data/feature'
+    tg_list = os.listdir(feature_path) ## 列出 tgfile_path 下的所有文件夹
+    tg_list.sort() ## 排序
+    arr_lab = []
+    c = 0 ## 计数功能
+    for i in tg_list:
+        lab_path = feature_path + '/' + i
+        arr_lab.append(lab_path)
+        c+=1
+    
+    X = arr_lab
+    kf = KFold(n_splits=10)
+    arr_ = []
+    for train, test in kf.split(X):
+        arr_tool = []
+        for i in range(train.size):
+            arr_tool.append(X[train[i]])
+        for i in range(test.size):
+            arr_tool.append(X[test[i]])
+    # return train,test
+        arr_.append(arr_tool)
+    arr_ = np.array(arr_)
+    return arr_
+
+
+if __name__ == "__main__":
+
+    # # arr_lab = k_cross_lab()
+    # # print(arr_lab)
+   
+    # # x,y =  k_cross()
+    # # print(x,y)
+    # k_cross = k_cross()
+    # print(k_cross)
+    # x = k_cross_lab()
+    x1 = k_cross()
+    print(x1)
+    print('yunxing')
+    pass
